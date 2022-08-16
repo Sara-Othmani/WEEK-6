0856887038e61eafac0d875682750fc60e4038e7
Sara-Othmani <107689444+Sara-Othmani@users.noreply.github.com>
2022-08-16 22:07:06 +0100

    Add files via upload

---
+++ YAML.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 21:56:10 2022
+
+@author: Administrateur
+"""
+
+from pyaml import yaml
+
+import pandas as pd
+df = pd.read_csv("Glacier/artists.csv")
+
